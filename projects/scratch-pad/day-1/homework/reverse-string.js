033596ee7c3fb5a915caa29239934a22165598a1
Tom Block <ltomblock@gmail.com>
2022-09-27 15:59:41 +0000

    Day1 Final

--- projects/scratch-pad/day-1/homework/reverse-string.js
+++ projects/scratch-pad/day-1/homework/reverse-string.js
@@ -18,15 +18,43 @@
  *  c. how do you access individual characters of a String?
  *  d. how do you concatenate Strings? What operator do we use?
  */
+/*
+I: a string
+O: a new string that is the reverse of old string
+C:must be string
+E:not a string or empty string
+*/
 
-function reverseString(input) {
+
+function reverseString(input){
     // YOUR CODE GOES BELOW HERE //
-    
+    // create empty array to temp store reverse string
+    let rString = [];
+     //create temp array to hold initial string input
+    let tempInput = input.split('');
+    // create exception that test that input is a string and not empty
+    if (typeof input != "string" || input === ""){
+      return "not a valid input";
+    }
+   //if string is a string and isnt an empty string create a loop and reverse it  
+   else{
+    for (let i = 0; i < tempInput.length; i++){
+     rString.unshift(tempInput[i]);
+    }}
+  // create new string from the rstring array
+  let newStr = rString.join("");
+  // return new string
+  return newStr;
+   }
+   
+     
+
+     
     
     
     
     // YOUR CODE GOES ABOVE HERE //
-}
+
 
 
 
