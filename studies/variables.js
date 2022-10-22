4d930cbff340c4e64b2517ac8542774f8e593304
Tom Block <ltomblock@gmail.com>
2022-10-22 19:18:13 +0000

    sat

---
+++ studies/variables.js
@@ -0,0 +1,12 @@
+/**
+ * Variables
+ * Variables are a way to reference a place in memory, where a value is stored, with a human redable word or
+ * or words. Variables are statment and the values they hold are expresions.
+ * There are three commenly used 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
