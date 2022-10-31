f15ecc40b21f30e5622b1b341852e586038ec3d3
Tom Block <ltomblock@gmail.com>
2022-10-31 00:26:39 +0000

    data

--- studies/datatypes.js
+++ studies/datatypes.js
@@ -31,6 +31,52 @@ console.log(undf); //Undefined
 //that returns object when called with typeOf
 console.log(typeof(null)) // 'object
 //NaN is a type that occurs when a mathematical operation cant return a number
-//Infinity and Negative Infinity
+//Infinity and Negative Infinity: these are similar to the ideas of infinity and
+//negative infinity in math.  When a number is too large of a value it will return infinity
+//or if negative negative infinity
+let Num = Number.MAX_VALUE * 2;
+console.log(Num); //infinity
+//Complex Data Types
+/**
+ * All complex data types fall under the rubric of "Objects".  Its important to remember
+ * however that Objects, Arrays, and Functions are all objects in JavaScript.
+ * Objects are referenced by an identifier, which means that multiple variables can
+ * be assigned to the same reference without creating new spaces in memory.
+ * Unlike primitive data which is always reassigned a new place in memory when assigned
+ * Unlike primitive data types complex data types are mutable 
+ */
+//Functions
+//Functions make up the bulk of what we think of as the "program" in programing.
+//when they are declared their values are stored in memory but in order to be 
+//used by the program they must be executed or called.  They have optional parameters
+//that at runtime are substituted for 'arguments'
+function logged(parameter){
+    console.log(parameter)
+}
+//nothing is logged to the terminal
+logged('argument');//'argument' is logged to the terminal
+//Arrays
+// In JavaScript arrays are a list of values stored in an ordered fashion separated by commas and
+/** encapsulated with brackets. The values can be any data type and data types can be mixed. 
+ */
+let newArr = [1, 2, 3, {'object': true}, ['another array']];
+/**
+ * Arrays are 0 indexed which means the first item in the array is at the 0 index
+ * the elements in the array can be accessed through bracket notation
+ */
+console.log(newArr[0]) // 1
+/**
+ * Objects
+ * Objects are a list of key/value pairs.  They are not ordered in the same manner as Arrays
+ * they can be accessed thought dot notation or brackets, through their key properties.  Objects
+ * are encased in curly brackets and like Arrays values can be any data type although keys are
+ * inevitably strings. Functions that exist in objects are referred to as methods
+ */
+let newObj = {
+    prop : 'Value',
+    method : function(){ console.log("I'm a method")}
+}
+newObj.method()// logs "I'm a method"
+
 
 
